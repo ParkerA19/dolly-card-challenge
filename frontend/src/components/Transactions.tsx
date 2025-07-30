ef4bcb980e218f13b6689b66df7eaf428df5e669
pandrews <pandrews@g.hmc.edu>
2025-07-30 17:06:30 -0400

    added tailwind to frontend

---
+++ frontend/src/components/Transactions.tsx
@@ -0,0 +1,8 @@
+export function Transactions(): React.ReactElement {
+  return (
+    <div>
+      <h2>Transactions Component</h2>
+      <p>This is where transaction-related content will go.</p>
+    </div>
+  )
+}
